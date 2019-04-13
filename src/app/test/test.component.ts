344edbdedb8f7e1a06332090697431d96023c909
choniyuazwan <choniyuazwan@gmail.com>
2019-04-14 00:56:57 +0700

    Angular 7 Tutorial - 8 - Style Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,35 +6,21 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{name}}
     </h2>
-    <h2 class="text-success">Azwan</h2>
-    <h2 [class]="successClass">Azwan</h2>
-
-    <h2 class="successClass" [class]="successClass">Azwan</h2>
-    <h2 [class.text-danger]="hasError">Azwan</h2>
-    <h2 [ngClass]="messageClasses">Azwan</h2>
+    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
+    <h2 [style.color]="highlightColor">Style Binding 2</h2>
+    <h2 [ngStyle]="titleStyles">Style Binding 3</h2>
   `,
-  styles: [`
-    .text-success {
-      color: green;
-    }
-    .text-danger {
-      color: red;
-    }
-    .text-special {
-      font-style: italic;
-    }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   public name =  "Azwan";
-  public successClass = "text-success";
-  public hasError = true;
+  public hasError = false;
   public isSpecial = true;
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
   }
 
   constructor() { }
