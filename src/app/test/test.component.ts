5cc30efe508602bf28c06b460056b495f8afd4fe
choniyuazwan <choniyuazwan@gmail.com>
2019-04-13 21:54:16 +0700

    Angular 7 Tutorial - 5 - Interpolation

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <h2>
+      {{2+2}}
+    </h2>
+    <h2>
+      {{"Welcome " + name}}
+    </h2>
+    <h2>{{name.length}}</h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{siteUrl}}</h2>
+  `,
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  public name =  "Codevolution";
+  public siteUrl = window.location.href;
+  greetUser() {
+    return "Hello" + this.name;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
