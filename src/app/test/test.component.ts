b7a823f9123eba5d71e2964fdc87c36fa5d56f1c
choniyuazwan <choniyuazwan@gmail.com>
2019-04-14 11:45:21 +0700

    Angular 7 Tutorial - 12 - ngIf Directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,14 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <input [(ngModel)]="name" type="text">
-    {{name}}
+    <!-- 
+    <h2 *ngIf="displayName; else elseBlock">
+      Azwan
+    </h2>
+
+    <ng-template #elseBlock>
+      <h2>Name is hidden</h2>
+    <ng-template>
+    -->
+
+    <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+
+    <ng-template #thenBlock>
+      <h2>Azwan</h2>
+    </ng-template>
+
+    <ng-template #elseBlock>
+      <h2>Name is hidden</h2>
+    </ng-template>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name =  "";
+  public displayName = true;
 
   constructor() { }
 
