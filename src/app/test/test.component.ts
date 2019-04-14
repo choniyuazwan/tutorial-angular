ec8baa1ff8f51e443405b7af4a305b82affb0e2a
choniyuazwan <choniyuazwan@gmail.com>
2019-04-14 16:57:44 +0700

    Angular 7 Tutorial - 16 - Pipes

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,20 +3,41 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <!-- <h2>{{"Hello " + parentData}}</h2> -->
-    <h2>{{"Hello " + name}}</h2>
-    <button (click)="fireEvent()">Send Event</button>
+    <h2>{{ name }}</h2>
+    <h2>{{ name | lowercase }}</h2>
+    <h2>{{ name | uppercase }}</h2>
+    <h2>{{ message | titlecase }}</h2>
+    <h2>{{ name | slice:3:7 }}</h2>
+    <h2>{{ person | json }}</h2>
+
+    <h2>{{ 5.678 | number:'1.2-3' }}</h2>
+    <h2>{{ 5.678 | number:'2.3-4' }}</h2>
+    <h2>{{ 5.678 | number:'2.4-5' }}</h2>
+    <h2>{{ 5.678 | number:'3.1-2' }}</h2>
+
+    <h2>{{ 0.25 | percent }}</h2>
+
+    <h2>{{ 0.25 | currency }}</h2>
+    <h2>{{ 0.25 | currency: 'EUR' }}</h2>
+    <h2>{{ 0.25 | currency: 'EUR' : 'code' }}</h2>
+
+    <h2>{{ date }}</h2>
+    <h2>{{ date | date:'short' }}</h2>
+    <h2>{{ date | date:'shortDate' }}</h2>
+    <h2>{{ date | date:'shortTime' }}</h2>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  // @Input() public parentData;
-  @Input('parentData') public name;
-  @Output() public childEvent = new EventEmitter();
-  fireEvent() {
-    this.childEvent.emit('Hey Azwan');
+  public name = "Paw Ang Ular";
+  public message = "Welcome to Paw Ang Ular";
+  public person = {
+    "firstName": "Choniyu",
+    "lastName": "Azwan"
   }
+  public date = new Date();
+
   constructor() { }
 
   ngOnInit() {
