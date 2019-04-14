8e0c71125684485d70a44c7d24bc25c70f35bec4
choniyuazwan <choniyuazwan@gmail.com>
2019-04-14 11:15:43 +0700

    Angular 7 Tutorial - 11 - Two Way Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,22 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
-      Welcome {{name}}
-    </h2>
-    <input #myInput type="text">
-    <button (click)="logMessage(myInput.value)">Log 1</button>
-    <button (click)="logMessage(myInput)">Log 2</button>
+    <input [(ngModel)]="name" type="text">
+    {{name}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name =  "Azwan";
-  logMessage(value) {
-    console.log(value);
-  }
-  
+  public name =  "";
+
   constructor() { }
 
   ngOnInit() {
