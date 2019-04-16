ad05a324bd58f8f954cff6ada1db6b8bb71b487c
choniyuazwan <choniyuazwan@gmail.com>
2019-04-16 23:48:51 +0700

    Angular 7 Tutorial - 28 - Relative Navigation

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -40,6 +40,7 @@ export class DepartmentDetailComponent implements OnInit {
 
   gotoDepartments() {
     let selectedId = this.departmentId ? this.departmentId : null;
-    this.router.navigate(['/departments', {id: selectedId, test: 'testValue'}]);
+    // this.router.navigate(['/departments', {id: selectedId, test: 'testValue'}]);
+    this.router.navigate(['../', {id: selectedId}], {relativeTo: this.route});
   }
 }
