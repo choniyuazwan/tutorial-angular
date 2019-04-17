573f30e16ff24faebe817a379fca7b2a3aa519f2
choniyuazwan <choniyuazwan@gmail.com>
2019-04-17 10:47:16 +0700

    update employee.service.ts

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -3,6 +3,12 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { IEmployee } from './employee';
 import { Observable } from 'rxjs';
 
+// import {throwError as observableThrowError,  Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+// import { IEmployee } from './employee';
+// import { tap, catchError } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,3 +25,17 @@ export class EmployeeService {
     return Observable.throw(error.message || "Server error");
   }
 }
+
+// @Injectable()
+// export class EmployeeService {
+//   private _url: string = "/assets/data/employees.json";
+//   constructor(private http:HttpClient) { }
+
+//   getEmployees(): Observable<IEmployee[]>{
+//     return this.http.get<IEmployee[]>(this._url)
+//                     .pipe(tap(data => alert(JSON.stringify(data))) , catchError(this.errorHandler))
+//   }
+//   errorHandler(error: HttpErrorResponse){
+//     return observableThrowError(error.message || "Server Error");
+//   }
+// }
